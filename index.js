1c1d98a22854b095d578619f686c444a78230203
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-03-10 09:42:47 +0100

    obsolete

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var path = require('path')
 var pojo = require('pojo-ops')
 
+// in need of a better model
+
 function store (files) {
   var o = {}
   var doublesplits = files.map(function (filepath) {
     return filepath.split(path.sep)
   })
   doublesplits.forEach(function (splits) {
-    var prev = {}
+    var prev = o
     splits.forEach(function (level, i, arr) {
       if (i === (arr.length - 1))
       if (!pojo.hasKeys(prev, level)) prev[level] = {}
